efaf6fd8d331a61cb3ce2d1aceebc02dd00cd1c2
Christopher Harrison <cnharrison@gmail.com>
2019-02-05 21:53:18 -0500

    1 mouseover fun-bus.jpg to change to evil bus image 2 mouseleave to change back

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 // Your code goes here
 // change for initial commit
+
+const mainImg = document.querySelector(".intro img");
+console.log(mainImg);
+
+mainImg.addEventListener("mouseover", event => {
+  mainImg.setAttribute(
+    "src",
+    "http://wp.penhallpublishing.com/wp-content/uploads/2014/09/routepromo.jpg"
+  );
+});
+
+mainImg.addEventListener("mouseleave", event => {
+    mainImg.setAttribute(
+      "src",
+      "img/fun-bus.jpg"
+    );
+  });
